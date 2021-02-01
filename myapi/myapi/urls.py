5e0c539dc6d04c7cd798ff9515e64b05bcf2e480
patriciatozi <ptrc.tozi@hotmail.com>
2021-01-31 23:52:12 -0300

    models creation

--- myapi/myapi/urls.py
+++ myapi/myapi/urls.py
@@ -19,6 +19,13 @@ from django.urls import path
 # Use include() to add URLS from the catalog application and authentication system
 from django.urls import include
 
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
@@ -27,14 +34,8 @@ urlpatterns += [
     path('myapp/', include('myapp.urls')),
 ]
 
-# Use static() to add url mapping to serve static files during development (only)
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-#Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='myapp/', permanent=True)),
 ]
--- myapi/myapp/models.py
+++ myapi/myapp/models.py
@@ -1,3 +1,96 @@
 from django.db import models
 
-# Create your models here.
+# Used to generate URLs by reversing the URL urlpatterns
+from django.urls import reverse
+
+# Required for unique shoes instances
+import uuid
+
+class Color(models.Model):
+
+    """Model representing the color of shoes"""
+    name = models.Charfield(max_length=30, help_text='Enter a color (e.g. blue)')
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.name
+
+
+class Type(models.Model):
+
+    """Model representing the type of shoes, feminine/masculine"""
+    name = models.Charfield(max_length=10, help_text='Feminine or masculine shoes')
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.name
+
+
+class Style(models.Model):
+
+    """Model representing the style of shoes"""
+    name = models.Charfield(max_length=50, help_text='Enter the style of shoes (e.g. sandals)')
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.name
+
+
+class Brand(models.Model):
+
+    """Model representing the brand of shoes"""
+    name = models.Charfield(max_length=100, help_text='Enter the brand of shoes')
+    country = models.Charfield(max_length=100)
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.name
+
+class Shoe(models.Model):
+
+    """Model representing the pair of shoes (not the product itself)"""
+    name = models.Charfield(max_length=100)
+    price = models.DecimalField(decimal_places=2)
+    size = models.IntegerField()
+    color = models.ManyToManyField('Color', help_text='Select a color for these shoes')
+    type = models.ForeignKey('Type', on_delete=models.SET_NULL, null=True)
+    style = models.ForeignKey('Style', on_delete=models.SET_NULL, null=True)
+    brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.name
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this pair of shoes"""
+        return reverse('shoes-detail', args=[str(self.id)])
+
+
+class ShoeInstance(models.Model):
+
+    """Model representing a specific pair of shoes"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular pair of shoes')
+    shoe = models.ForeignKey('Shoe', on_delete=models.RESTRICT)
+    post_date = models.DateField(null=True, blank=True)
+
+    PURCHASE_STATUS = (
+        ('a', 'Available'),
+        ('s', 'In stock'),
+        ('n', 'Not available'),
+    )
+
+    status = models.Charfield(
+        max_length=1,
+        choices=PURCHASE_STATUS,
+        blank=True,
+        default='a',
+        help_text="Shoes availability",
+    )
+
+    class Meta:
+        # Ordering by the lastest products posted
+        ordering = ['-post_date']
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return f'{self.id} ({self.shoe.name})'
