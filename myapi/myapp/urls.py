dc830adeb6620145b13a7057db61beacdf30713d
patriciatozi <ptrc.tozi@hotmail.com>
2021-02-21 03:24:55 -0300

    fixing link to pages

--- myapi/myapp/urls.py
+++ myapi/myapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('feminine_shoes/', views.feminine_shoes, name='feminine_shoes'),
     path('masculine_shoes/', views.masculine_shoes, name='masculine_shoes'),
-    path('<int:id>', views.shoes_detail, name='shoes_detail'),
+    path('shoe/<int:id>', views.shoes_detail, name='shoes_detail'),
 ]
