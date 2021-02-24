3ac73d19d043daa2eac2648c2fb5e4f69d02e17f
patriciatozi <ptrc.tozi@hotmail.com>
2021-02-23 22:20:19 -0300

    customizing add product form

--- myapi/myapp/forms.py
+++ myapi/myapp/forms.py
@@ -6,3 +6,6 @@ class ShoeForm(forms.ModelForm):
     class Meta:
         model = Shoe
         fields = '__all__'
+        labels = {'name': "Nome", 'price': "Preço", 'size': "Tamanho",
+                  'style': "Estilo", 'type': "Tipo", 'color': "Cor",
+                  'brand': "Marca", 'post_date': "Data", 'status':"Status", 'image':"Imagem"}
--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Shoe
 from .forms import ShoeForm
@@ -37,6 +38,22 @@ def shoes_detail(request, id):
 
 def add_shoes(request):
 
-    form = ShoeForm()
+    if request.method == "POST":
+
+        form = ShoeForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            form.save()
+            print('sucess')
+
+            return HttpResponseRedirect(reverse('myapp:index'))
+
+        else:
+
+            print('failure')
+    else:
+
+        form = ShoeForm()
 
     return render(request, 'add_shoes.html', {'form': form})
