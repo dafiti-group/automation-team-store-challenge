2700c447ab58bbb9989fc7e133650999836fd1d2
Nayanna Nara <nayanna501@gmail.com>
2021-04-04 17:43:36 -0300

    static root

--- dafiti_challenge/settings.py
+++ dafiti_challenge/settings.py
@@ -152,24 +152,24 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-        'static'
-] 
-
 # STATIC_URL = '/static/'
-# if DEBUG:
-#    STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static'),
-#    ]
-# else:
-#    STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = [
+#         'static'
+# ] 
+
+STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRONJOBS = [
     ('*/60 * * * *', 'core.cron.scraping')
