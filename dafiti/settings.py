8153a97e9083e2f9a7d984d89ee1499684aa7271
Filipe Rafaeli <filiperafaeli@live.com>
2021-04-26 10:57:03 -0300

    config para heroku

--- dafiti/settings.py
+++ dafiti/settings.py
@@ -54,6 +54,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dafiti.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
