c85248bf11b989087c84745a923f34101e480114
CHRISTIAN MASSAAKI OSKATA <kira26107@gmail.com>
2021-07-12 18:57:19 -0300

    requirements

--- mysite/shoes/urls.py
+++ mysite/shoes/urls.py
@@ -3,4 +3,3 @@ from .views import ShoesViewSet
 
 shoes_router = routers.DefaultRouter()
 shoes_router.register("shoes", viewset=ShoesViewSet, basename="shoes")
-
--- mysite/shoes/views.py
+++ mysite/shoes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Shoes
@@ -10,3 +11,8 @@ class ShoesViewSet(ModelViewSet):
     serializer_class = ShoesSerializer
     queryset = Shoes.objects.all().order_by("-data_de_lancamento")
     pagination_class = PageNumberPagination
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = ShoesSerializer(queryset, many=True)
+        return Response(serializer.data)
