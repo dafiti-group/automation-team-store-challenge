140fcd5f5b1dfbea799e64f692aba5cfd6f3c64e
CHRISTIAN MASSAAKI OSKATA <kira26107@gmail.com>
2021-07-12 19:40:33 -0300

    att model

--- mysite/shoes/models.py
+++ mysite/shoes/models.py
@@ -1,8 +1,9 @@
+from django.contrib.postgres.fields import IntegerRangeField
 from django.db import models
 
 from django.db import models
 from django.utils.timezone import now
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Shoes(models.Model):
     class ShoesStatus(models.TextChoices):
@@ -16,6 +17,10 @@ class Shoes(models.Model):
     data_de_lancamento = models.DateTimeField(default=now, editable=True)
     marca = models.CharField(max_length=30)
     quantidade = models.IntegerField(default=10)
+    preco = models.DecimalField(max_digits=5, decimal_places=2, default=100.00)
+    tamanho = models.IntegerField(validators=[MinValueValidator(0),
+                                           MaxValueValidator(50)],
+                                  default=40)
 
     def __str__(self) -> str:
         return f"{self.modelo}"
--- mysite/shoes/tests/test_shoes.py
+++ mysite/shoes/tests/test_shoes.py
@@ -29,7 +29,6 @@ def test_one_shoes_exists_should_succedd(client) -> None:
     assert response_content.get("marca") == ""
     assert response_content.get("quantidade") == 10
 
-
 # ------------- Test Post Shoes ---------------------
 
 def test_create_shoes_without_arguments_should_fail(client) -> None:
@@ -62,7 +61,7 @@ def test_create_existing_shoes_without_marca_should_fail(client) -> None:
         ]
     }
 
-#@pytest.mark.skip
+#@pytest.mark.modelo
 def test_create_shoes_with_only_modelo_all_fields_should_be_default(client) -> None:
     response = client.post(path=shoes_url, data={"modelo": "teste shoes", "marca":"shoes"})
     assert response.status_code == 201
@@ -72,6 +71,7 @@ def test_create_shoes_with_only_modelo_all_fields_should_be_default(client) -> N
     assert response_content.get("marca") == "shoes"
     assert response_content.get("quantidade") == 10
 
+@pytest.mark.status
 def test_create_shoes_with_esgotado_status_should_succeed(client) -> None:
 
     response = client.post(
@@ -85,3 +85,88 @@ def test_create_shoes_with_esgotado_status_should_succeed(client) -> None:
     assert response_content.get("marca") == "shoes"
     assert response_content.get("quantidade") == 10
 
+@pytest.mark.preco
+def test_one_shoes_with_preco_out_ranged_should_fail(client) -> None:
+
+    response = client.post(path=shoes_url, data={"modelo": "Googleshoes", "marca": "shoes", "preco":"999999.999"})
+    assert response.status_code == 400
+    assert json.loads(response.content) == {
+
+        'preco': [
+            'Ensure that there are no more than 5 digits in total.'
+        ]
+
+    }
+
+@pytest.mark.preco
+def test_create_shoes_with_preco_in_range_status_should_succeed(client) -> None:
+
+    response = client.post(
+        path=shoes_url, data={"modelo": "teste shoes", "preco": "999.99","marca":"shoes"}
+    )
+    assert response.status_code == 201
+
+    response_content = json.loads(response.content)
+    assert response_content.get("modelo") == "teste shoes"
+    assert response_content.get("status") == "Disponivel"
+    assert response_content.get("marca") == "shoes"
+    assert response_content.get("quantidade") == 10
+    assert float(response_content.get("preco")) == 999.99
+
+@pytest.mark.preco
+def test_create_shoes_without_preco_should_succeed(client) -> None:
+    response = client.post(
+        path=shoes_url, data={"modelo": "teste shoes",  "marca": "shoes"}
+    )
+    assert response.status_code == 201
+
+    response_content = json.loads(response.content)
+    assert response_content.get("modelo") == "teste shoes"
+    assert response_content.get("status") == "Disponivel"
+    assert response_content.get("marca") == "shoes"
+    assert response_content.get("quantidade") == 10
+    assert float(response_content.get("preco")) == 100.00
+
+@pytest.mark.tamanho
+def test_create_shoes_without_preco_should_succeed(client) -> None:
+    response = client.post(
+        path=shoes_url, data={"modelo": "teste shoes",  "marca": "shoes"}
+    )
+    assert response.status_code == 201
+
+    response_content = json.loads(response.content)
+    assert response_content.get("modelo") == "teste shoes"
+    assert response_content.get("status") == "Disponivel"
+    assert response_content.get("marca") == "shoes"
+    assert response_content.get("quantidade") == 10
+    assert float(response_content.get("preco")) == 100.00
+    assert int(response_content.get("tamanho")) == 40
+
+@pytest.mark.tamanho
+def test_create_shoes_with_preco_in_range_status_should_succeed(client) -> None:
+
+    response = client.post(
+        path=shoes_url, data={"modelo": "teste shoes", "tamanho": "50","marca":"shoes"}
+    )
+    assert response.status_code == 201
+
+    response_content = json.loads(response.content)
+    assert response_content.get("modelo") == "teste shoes"
+    assert response_content.get("status") == "Disponivel"
+    assert response_content.get("marca") == "shoes"
+    assert response_content.get("quantidade") == 10
+    assert float(response_content.get("preco")) == 100.0
+    assert int(response_content.get("tamanho")) == 50
+
+@pytest.mark.tamanho
+def test_one_shoes_with_tamanho_out_ranged_should_fail(client) -> None:
+
+    response = client.post(path=shoes_url, data={"modelo": "Googleshoes", "marca": "shoes", "tamanho":"99"})
+    assert response.status_code == 400
+    assert json.loads(response.content) == {
+
+        'tamanho': [
+            'Ensure this value is less than or equal to 50.'
+        ]
+
+    }
