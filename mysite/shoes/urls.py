3435d92825e5bf03003cd777ab117e4470fcbd37
CHRISTIAN MASSAAKI OSKATA <kira26107@gmail.com>
2021-07-12 18:45:02 -0300

    Update urls.py

--- mysite/shoes/urls.py
+++ mysite/shoes/urls.py
@@ -1,12 +1,6 @@
 from rest_framework import routers
-from rest_framework_simplejwt.views import (TokenObtaionPairView, TokenRefreshView)
 from .views import ShoesViewSet
 
 shoes_router = routers.DefaultRouter()
 shoes_router.register("shoes", viewset=ShoesViewSet, basename="shoes")
 
-from django.urls import path
-
-urlpatterns = [
-    path('posts/', ShoesView.as_view(), name='posts_view')
-]
